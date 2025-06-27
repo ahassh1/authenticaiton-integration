f52a2218dc23bf2876056c68e25ba33644d2ed2a
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-28 03:17:09 +0600

    input data added in clg

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Register = () => {
+    const handleRegister= e =>{
+        e.preventDefault()
+        const name = e.target.name.value;
+        const photoURL = e.target.photoURL.value;
+        const email = e.target.email.value;
+        const password= e.target.password.value;
+        console.log(name , photoURL, email, password);
+    }
     return (
     <div className="card mx-auto bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-10">
       <h1 className="text-3xl text-center font-bold">Register now!</h1>
       <div className="card-body">
-        <form className="fieldset">
+        <form onClick={handleRegister} className="fieldset">
                 <label className="label">Name</label>
-          <input type="text" className="input" placeholder="name" />
+          <input type="text" name='name' className="input" placeholder="name" />
               <label className="label">Photo URL</label>
-          <input type="text" className="input" placeholder="photo URL" />
+          <input type="text" name='photoURL' className="input" placeholder="photo URL" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
