463c9c548d44c0416c78d764be436ec6eeb143e7
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-01 12:42:39 +0600

    procedure of useEffect hook

--- src/__Auth__integration.js
+++ src/__Auth__integration.js
@@ -2,6 +2,9 @@
  * 1. create a context, so that we can share the state of user across the application 
  *  
  * 2. create a provider to re-use the context 
- * 3. set CreateUser via context 
+ * 3. set CreateUser via context provided functoin
  *  
+ * 4. login user via context provided funciton 
+ * 5. observe : one single place (inside the provider) ==> if the user state on firebase changes you can update the user information in your  
+ * 
  */
